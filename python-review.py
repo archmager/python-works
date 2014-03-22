79ce61b9ebb3cb06966086297d415ee75ede872d
archmager <archmager@126.com>
2014-03-22 20:49:14 +0800

    class

--- python-review.py
+++ python-review.py
@@ -14,3 +14,17 @@ for i, c in enumerate(foo):
 #列表解析
 for i in [x**2 for x in range(1,10) if not x%2]:
 	print i
+
+#类和对象
+class FooClass(object):
+	version = "1.0.0"
+	def __init__(self, name='default'):
+		self.name = name
+	def setName(self, name):
+		self.name = name
+	def getName(self):
+		return self.name
+
+foo1 = FooClass('sam')
+print foo1.version
+print foo1.getName()
