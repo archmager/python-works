e5062265d7ae6f680cfefb4d1388830998385e36
archmager <archmager@126.com>
2014-04-04 17:36:49 +0800

    快速登陆开发机

---
+++ goto-machine.py
@@ -0,0 +1,18 @@
+#_*_　coding:utf-8 _*_
+
+import sys
+import os
+
+
+if __name__ == "__main__":
+	goto = sys.argv[1]
+	print goto
+	pofix = {"ttstw" : "l-ttstw%s.f.beta.cn6", "dev" : "l-ttstw%s.f.dev.cn6", "test" : "l-ttstest%s.f.beta.cn6"}
+	for (k, v) in pofix.items():
+		print k
+		print v
+		if goto.find(k) >= 0:
+			num = goto.replace(k, "")
+			print v % (num)
+			os.system("ssh " + v % (num))
+
